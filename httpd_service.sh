a9e2f1bdbad081c22e9093d85c99cce7594cbdf5
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-12 09:26:40 +0000

    added the httpd_service.sh

--- httpd_service.sh
+++ httpd_service.sh
@@ -19,6 +19,25 @@ then
         fi
 else
 
-    echo "Sorry, you dont have privileges to run the docker service as you are not root user"
+    if sudo -v 1>/dev/null 2>/dev/null
+    then
+               if systemctl status httpd 1>/dev/null 2>/dev/null
+            
+               then
+            
+                echo  "HTTPD service is already running"
+              
+               else
+               
+                echo "Strting the httpd"
+                systemctl start httpd
+                echo "Started the httpd"
+               
+            fi
+    else
+
+    echo "Sorry, you dont have privileges to run the docker service as you are not root user and you dont have sudo permissions"
+    
+    fi
     
 fi
