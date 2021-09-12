62e3633914992283d16d0d33aea9521a821cd020
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-12 07:48:46 +0000

    added the rootornot file

--- rootornot.sh
+++ rootornot.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-userid=$(id -u)
+#userid=$(id -u)
 
-[[ $userid == 0 ]] && echo "You are root user"  ||  echo "You are not root user"
+#[[ $userid == 0 ]] && echo "You are root user"  ||  echo "You are not root user"
 
+#above are 2 lines or you can use below one line code
+
+#[[ $(id -u) -eq 0 ]] && echo "You are root user"  ||  echo "You are not root user"
+
+
+# instead of all above method, you can use if condition.
+
+if [ $(id -u) -eq 0 ]]
+then
+
+  echo "You are root user" 
+  
+else
+
+echo "You are not root user"
