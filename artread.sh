fd494bebf75b755fa0c7a5897284f7601d09da09
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-12 05:33:25 +0000

    added the 1st file in bash directory

---
+++ artread.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p " Enter the number 1" num1
+read -p " Enter the number 2 " num2
+#((sum=num1+num2))
+
+sum='bc<<<"scale=2;$num1+$num2"'
+
+echo " The addition of the given numbers is $sum"
+
